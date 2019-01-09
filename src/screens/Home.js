853d8300eff882eb959297652a4bb2faf9732dce
Sungjung <desjungchoi@gmail.com>
2019-01-09 16:21:11 +0900

    커스텀버튼

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,15 +1,72 @@
 import React,{Component} from "react";
 import {
   View,
+  SafeAreaView,
+  Image,
+  Dimensions,
+  StatusBar,
   Text,
   StyleSheet,
-  Button,
   ScrollView,
   TouchableOpacity
 } from "react-native";
 
 import GridView from 'react-native-super-grid';
-import {Navigation} from 'react-native-navigation'
+import { Button } from 'react-native-elements';
+import {Navigation} from 'react-native-navigation';
+
+
+const SCREEN_WIDTH = Dimensions.get('window').width;
+
+
+class CustomButton extends Component {
+  
+  constructor() {
+    super();
+
+    this.state = {
+      selected: false,
+    };
+  }
+
+  componentDidMount() {
+    const { selected } = this.props;
+
+    this.setState({
+      selected,
+    });
+  }
+
+  render() {
+    const { title } = this.props;
+    const { selected } = this.state;
+
+    return (
+      <Button
+        title={title}
+        titleStyle={{ fontSize: 15, color: 'white', fontFamily: 'regular' }}
+        buttonStyle={
+          selected
+            ? {
+                backgroundColor: 'rgba(213, 100, 140, 1)',
+                borderRadius: 100,
+                width: 127,
+              }
+            : {
+                borderWidth: 1,
+                borderColor: 'white',
+                borderRadius: 30,
+                width: 127,
+                backgroundColor: 'transparent',
+              }
+        }
+        containerStyle={{ marginRight: 10 }}
+        onPress={() => this.setState({ selected: !selected })}
+      />
+    );
+  }
+}
+
 
 class Home extends Component{
 
@@ -42,6 +99,54 @@ class Home extends Component{
       
       <ScrollView>
       {/* <View style={styles.container}> */}
+
+
+      <View style={{ flex: 1, width: SCREEN_WIDTH, marginTop: 20 }}>
+                  
+                  <ScrollView
+                    style={{ flex: 1 }}
+                    horizontal
+                    showsHorizontalScrollIndicator={false}
+                  >
+                    <View
+                      style={{
+                        flex: 1,
+                        flexDirection: 'column',
+                        height: 170,
+                        marginLeft: 40,
+                        marginRight: 10,
+                      }}
+                    >
+                      <View style={{ flex: 1, flexDirection: 'row' }}>
+                        <CustomButton title="Philosophy" selected={true} />
+                        <CustomButton title="Sport" selected={true} />
+                        <CustomButton title="Swimming" selected={true} />
+                        <CustomButton title="Religion" selected={true} />
+                      </View>
+
+                      <View style={{ flex: 1, flexDirection: 'row' }}>
+                        <CustomButton title="Music" selected={true} />
+                        <CustomButton title="Soccer" selected={true} />
+                        <CustomButton title="Radiohead" selected={true} />
+                        <CustomButton title="Micheal Jackson" selected={true} />
+                      </View>
+
+                      <View style={{ flex: 1, flexDirection: 'row' }}>
+                        <CustomButton title="Travelling" selected={true} />
+                        <CustomButton title="Rock'n'Roll" selected={true} />
+                        <CustomButton title="Dogs" selected={true} />
+                        <CustomButton title="France" selected={true} />
+                      </View>
+
+                    </View>
+
+                  </ScrollView>
+
+
+                </View>
+
+
+
       <GridView
         itemDimension={130}
         items={items}
@@ -51,7 +156,7 @@ class Home extends Component{
           <View>
            <TouchableOpacity
          style={[styles.itemContainer, { backgroundColor: item.code }]}
-         onPress={()=> this.goToScreen('ScrollViewP')}
+        //  onPress={()=> this.goToScreen('ScrollViewP')}
         >
         </TouchableOpacity>
           </View>
